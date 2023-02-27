037eca28401a7a3c5e747939ef0532e2959569d0
Denis Cavalli <denisgcavalli@gmail.com>
2023-02-27 18:19:54 +0100

    AccessControl with Roles

--- contracts/MomToken.sol
+++ contracts/MomToken.sol
@@ -4,22 +4,31 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MOMToken is ERC20, ERC20Burnable, Pausable, Ownable {
+contract MOMToken is ERC20, ERC20Burnable, Pausable, AccessControl {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    
     constructor(uint256 initialSupply) ERC20("Momentum", "MOM") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(BURNER_ROLE, msg.sender);
         _mint(msg.sender, initialSupply);
     }
 
-    function pause() public onlyOwner {
+    function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
@@ -29,5 +38,14 @@ contract MOMToken is ERC20, ERC20Burnable, Pausable, Ownable {
         uint256 amount
     ) internal override whenNotPaused {
         super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function burn(uint256 amount) public override onlyRole(BURNER_ROLE) {
+        _burn(_msgSender(), amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public override onlyRole(BURNER_ROLE) {
+        _spendAllowance(account, _msgSender(), amount);
+        _burn(account, amount);
     }
 }
--- test/MomToken.ts
+++ test/MomToken.ts
@@ -3,33 +3,78 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+function rolesRevertString(address: string, role: string): string {
+  return `AccessControl: account ${address.toLowerCase()} is missing role ${role.toLowerCase()}`
+}
+
 describe("MomToken", function () {
   async function deployMomTokenOneKSupply() {
     const initialSupply = 1000;
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner, addr0] = await ethers.getSigners();
 
     const MOMToken = await ethers.getContractFactory("MOMToken");
     const momToken = await MOMToken.deploy(initialSupply);
 
-    return { momToken, initialSupply, owner, otherAccount };
+    return { momToken, initialSupply, owner, addr0 };
   }
 
   describe("Deployment", function () {
-    it("Should set the right owner", async function () {
+    it("Should set all roles to the contract deployer", async function () {
       const { momToken, owner } = await loadFixture(deployMomTokenOneKSupply);
-
-      expect(await momToken.owner()).to.equal(owner.address);
+      
+      expect(await momToken.hasRole(momToken.DEFAULT_ADMIN_ROLE(), owner.address)).to.equal(true);
+      expect(await momToken.hasRole(momToken.BURNER_ROLE(), owner.address)).to.equal(true);
+      expect(await momToken.hasRole(momToken.MINTER_ROLE(), owner.address)).to.equal(true);
+      expect(await momToken.hasRole(momToken.PAUSER_ROLE(), owner.address)).to.equal(true);
     });
 
     it("Should set initial suply to owner", async function () {
       const { momToken, owner, initialSupply } = await loadFixture(
         deployMomTokenOneKSupply
       );
+      
+      const balance = await momToken.balanceOf(owner.address);
 
-      expect(await momToken.balanceOf(owner.address)).to.equal(
+      expect(balance).to.equal(
         initialSupply
       );
     });
 
+  });
+
+  describe("Roles", function () {
+    it("Should not burn or burnFrom tokens if address doesn't have the Burner role", async function () {
+      const { momToken, owner, addr0 } = await loadFixture(deployMomTokenOneKSupply);
+      const amount = 10;
+      // Lower case to match the revert message
+      const burnerRole = await momToken.BURNER_ROLE();
+
+      await momToken.transfer(addr0.address, amount);
+
+      await expect(momToken.connect(addr0).burn(amount)).to.be.revertedWith(rolesRevertString(addr0.address, burnerRole));
+      await expect(momToken.burn(amount)).to.emit(momToken, "Transfer").withArgs(owner.address, ethers.constants.AddressZero, amount);
+    });
+
+    it("Should not mint tokens if address doesn't have the Minter role", async function () {
+      const { momToken, owner, addr0 } = await loadFixture(deployMomTokenOneKSupply);
+      const amount = 10;
+      const minterRole = await momToken.MINTER_ROLE();
+
+      await expect(momToken.connect(addr0).mint(addr0.address, amount)).to.be.revertedWith(rolesRevertString(addr0.address, minterRole));
+      await expect(momToken.mint(addr0.address, amount)).to.emit(momToken, "Transfer").withArgs(ethers.constants.AddressZero, addr0.address, amount);
+    });
+
+    it("Should not pause and unpause contract if address doesn't have the Puaser role", async function () {
+      const { momToken, owner, addr0 } = await loadFixture(deployMomTokenOneKSupply);
+      const amount = 10;
+      const pauserRole = await momToken.PAUSER_ROLE();
+
+      await expect(momToken.connect(addr0).pause()).to.be.revertedWith(rolesRevertString(addr0.address, pauserRole));
+      await expect(momToken.pause()).to.emit(momToken, "Paused").withArgs(owner.address);
+
+      await expect(momToken.connect(addr0).unpause()).to.be.revertedWith(rolesRevertString(addr0.address, pauserRole));
+      await expect(momToken.unpause()).to.emit(momToken, "Unpaused").withArgs(owner.address);
+
+    });
   });
 });
