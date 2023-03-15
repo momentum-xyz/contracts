1e9b814957af200ede0e21a01bd9b817a7d28037
Denis Cavalli <denisgcavalli@gmail.com>
2023-03-15 16:51:59 +0100

    Improving docs with Natspec

--- contracts/MomToken.sol
+++ contracts/MomToken.sol
@@ -6,12 +6,17 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+/** 
+* @title MOM Token
+* @author Odyssey
+* @notice The Momentum Token
+*/
 contract MOMToken is ERC20, ERC20Burnable, Pausable, AccessControl {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    
+    /// Constructor of the contract
     constructor(uint256 initialSupply) ERC20("Momentum", "MOM") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
@@ -20,18 +25,33 @@ contract MOMToken is ERC20, ERC20Burnable, Pausable, AccessControl {
         _mint(msg.sender, initialSupply);
     }
 
+    /**
+    *  @notice Pauses the contract, no actions will be allowed until it is unpaused
+    *  @dev Only pauser can pause the contract
+    */
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
+    /**
+    *  @notice Unpause the contract
+    *  @dev Only pauser can unpause the contract
+    */
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
 
+    /**
+     * @notice Mint new tokens
+     * @dev Only admin and minter can perform this action
+     * @param to Destination of the new minted tokens
+     * @param amount Amount of tokens to be minted
+     */
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
+    /// @dev Only adding the 'whenNotPaused' modifier
     function _beforeTokenTransfer(
         address from,
         address to,
@@ -40,10 +60,21 @@ contract MOMToken is ERC20, ERC20Burnable, Pausable, AccessControl {
         super._beforeTokenTransfer(from, to, amount);
     }
 
+    /**
+     * @notice Burn the provided certain amount of tokens
+     * @dev Only burner can burn tokens
+     * @param amount Amount of tokens to be burned
+     */
     function burn(uint256 amount) public override onlyRole(BURNER_ROLE) {
         _burn(_msgSender(), amount);
     }
 
+    /**
+     * @notice Burn the provided amount of tokens from the provided account
+     * @dev Only burner can burn tokens
+     * @param account Address of the account that will have the tokens burned
+     * @param amount Amount of tokens to be burned
+     */
     function burnFrom(address account, uint256 amount) public override onlyRole(BURNER_ROLE) {
         _spendAllowance(account, _msgSender(), amount);
         _burn(account, amount);
