8c93f6e2b7860a3a8cf308050f8d957317499b4f
Denis Cavalli <denisgcavalli@gmail.com>
2023-04-03 18:37:09 +0200

    Enabling optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity:  {
+    version: "0.8.17",
+    settings: { 
+      optimizer: { 
+        enabled: true,
+        runs: 2
+      }
+    }
+  },
 };
 
 export default config;
