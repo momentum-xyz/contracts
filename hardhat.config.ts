9940927fbf0ce0557a651e3dc52c4c634ecd7949
Denis Cavalli <denisgcavalli@gmail.com>
2023-04-20 15:53:46 +0200

    solidity-docgen dep and hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import 'solidity-docgen';
 
 const config: HardhatUserConfig = {
   solidity:  {
@@ -10,8 +11,11 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 2
       }
-    }
+    },
   },
+  docgen: {
+    pages: 'files',
+  }
 };
 
 export default config;
