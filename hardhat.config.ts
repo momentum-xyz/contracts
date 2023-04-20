0f14c3f8cda6ca01ecfec62655dda4a79d8565be
Denis Cavalli <denisgcavalli@gmail.com>
2023-04-20 15:53:46 +0200

    solidity-docgen dep and hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'solidity-docgen';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  docgen: {
+    pages: 'files',
+  }
 };
 
 export default config;
